259dc139997d37bbf39e8370e87e474658367002
Paula Suarez <pauladzuck@hotmail.com>
2023-06-25 19:56:22 -0500

    Adding react bootstrap and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 
 const App = () => {
   return (
@@ -8,7 +10,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-        <p>welcome to proshop</p>
+     <Outlet />
+     
         </Container>
         <Footer />
       </main>
